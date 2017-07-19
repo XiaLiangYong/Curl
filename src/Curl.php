a28a78726742af69fb6c8925cb6682860c912320
liangyong <liangyong@rrkd.cn>
2017-07-19 14:14:56 +0800

    测试

---
+++ src/Curl.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace alibeibei\curl;
+
+use alibeibei\curl\driver\Request;
+
+/**
+ * Created by PhpStorm.
+ * User: xialiangyong
+ * Date: 2017/7/19
+ * Time: 14:07
+ */
+class Curl
+{
+    private static $instance = null;
+
+    private static function getInstance()
+    {
+        if (isset(self::$instance)) {
+            return self::$instance;
+        }
+        self::$instance = new Request();
+        return self::$instance;
+    }
+
+    public function __call($method, $params)
+    {
+        return call_user_func_array([self::$instance, $method], $params);
+    }
+
+
+    public static function __callStatic($method, $args)
+    {
+        return call_user_func_array([self::$instance, $method], $args);
+    }
+}
