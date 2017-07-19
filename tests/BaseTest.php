6ccda984820337927b9c8ca22e04d30a298bfa53
liangyong <liangyong@rrkd.cn>
2017-07-19 14:25:40 +0800

    测试

---
+++ tests/BaseTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Created by PhpStorm.
+ * User: xialiangyong
+ * Date: 2017/7/19
+ * Time: 14:24
+ */
+class BaseTest extends TestCase
+{
+
+}
