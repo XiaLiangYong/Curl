1766d6d65fc4f3cebd97444d2cadd11344ad3cd2
liangyong <liangyong@rrkd.cn>
2017-07-19 15:43:29 +0800

    测试

--- src/Curl.php
+++ src/Curl.php
@@ -9,6 +9,17 @@ use alibeibei\curl\driver\Request;
  * User: xialiangyong
  * Date: 2017/7/19
  * Time: 14:07
+ * @method get(String $url) get请求
+ * @method post(String $name, array $postData) post请求
+ * @method setCookie(String $cookies) 设置cookies
+ * @method setProxy(String $proxy) 设置代理
+ * @method setTime(String $time) 设置超时时间
+ * @method setHeaders(array $headers) 设置header头
+ * @method setIsHeader(bool $status) 设置是否需要返回头信息
+ * @method setUserAgent(String $useragent) 设置userAget信息
+ * @method getResult(String $result) 获取返回的完整信息
+ * @method getCode(String $code) 获取返回的请求状态码
+ * @method getResHeader(String $header) 获取返回的headers
  */
 class Curl
 {
@@ -23,6 +34,11 @@ class Curl
         return self::$instance;
     }
 
+    /**
+     * @param $method
+     * @param $params
+     * @return mixed
+     */
     public function __call($method, $params)
     {
         return call_user_func_array([self::getInstance(), $method], $params);
--- src/driver/Request.php
+++ src/driver/Request.php
@@ -10,8 +10,75 @@ namespace alibeibei\curl\driver;
  */
 class Request
 {
-    protected $code;
-    protected $result;
+    private $code;//代码请求code状态码
+    private $result;//返回的body
+    private $isHeader;//是否需要返回header
+    private $resHeader;//返回的header
+    private $headers = '';//初始化的headers
+    private $userAgent;//useragent
+    private $time = 30;//超时时间
+    private $proxy = '';//代理
+    private $cookie;//原始的cookie
+
+
+    /**
+     * @param mixed $cookie
+     */
+    public function setCookie($cookie)
+    {
+        $this->cookie = $cookie;
+    }
+
+    /**
+     * @param string $proxy
+     */
+    public function setProxy($proxy)
+    {
+        $this->proxy = $proxy;
+    }
+
+
+    /**
+     * @param int $time
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+    }
+
+    /**
+     * @param mixed $setHeader
+     */
+    public function setHeaders($headers)
+    {
+        $this->headers = $headers;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResHeader()
+    {
+        return $this->resHeader;
+    }
+
+
+    /**
+     * @param mixed $isHeader
+     */
+    public function setIsHeader($isHeader)
+    {
+        $this->isHeader = $isHeader;
+    }
+
+
+    /**
+     * @param mixed $userAgent
+     */
+    public function setUserAgent($userAgent)
+    {
+        $this->userAgent = $userAgent;
+    }
 
     /**
      * @return mixed
@@ -21,13 +88,6 @@ class Request
         return $this->result;
     }
 
-    /**
-     * @param mixed $result
-     */
-    public function setResult($result)
-    {
-        $this->result = $result;
-    }
 
     /**
      * @return mixed
@@ -35,14 +95,6 @@ class Request
     public function getCode()
     {
         return $this->code;
-    }
-
-    /**
-     * @param mixed $code
-     */
-    public function setCode($code)
-    {
-        $this->code = $code;
     }
 
 
@@ -58,6 +110,30 @@ class Request
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
+        //是否需要返回头
+        if ($this->isHeader) {
+            curl_setopt($ch, CURLOPT_HEADER, 1);
+        }
+        //设置请求头
+        if ($this->headers) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        }
+        //设置代理
+        if ($this->proxy) {
+            curl_setopt($ch, CURLOPT_PROXY, $this->proxy);
+        }
+        //设置userAgent
+        if ($this->userAgent) {
+            curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent);
+        }
+        //设cookie
+        if ($this->cookie) {
+            curl_setopt($ch, CURLOPT_COOKIE, $this->cookie);
+        }
+        //设置超时时间
+        if ($this->time) {
+            curl_setopt($ch, CURLOPT_TIMEOUT, $this->time);
+        }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
@@ -67,11 +143,21 @@ class Request
         } else {
             $data = curl_multi_getcontent($ch);
         }
-        $this->setCode(curl_getinfo($ch, CURLINFO_HTTP_CODE));
+        $this->result = $data;
+        $this->code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
-        $this->setResult($data);
-
-        return $data;
+        $res = '';
+        if ($this->isHeader) {
+            $data = explode("\r\n\r\n", $data);
+            if (count($data > 1)) {
+                $resHeader = $data[0];
+                $res = $data[1];
+                $this->resHeader = $resHeader;
+            }
+        } else {
+            $res = $data;
+        }
+        return $res;
     }
 
     /**
@@ -87,10 +173,33 @@ class Request
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
+        //是否需要返回头
+        if ($this->isHeader) {
+            curl_setopt($ch, CURLOPT_HEADER, 1);
+        }
+        //设置请求头
+        if ($this->headers) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        }
+        //设置代理
+        if ($this->proxy) {
+            curl_setopt($ch, CURLOPT_PROXY, $this->proxy);
+        }
+        //设置userAgent
+        if ($this->userAgent) {
+            curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent);
+        }
+        //设cookie
+        if ($this->cookie) {
+            curl_setopt($ch, CURLOPT_COOKIE, $this->cookie);
+        }
+        //设置超时时间
+        if ($this->time) {
+            curl_setopt($ch, CURLOPT_TIMEOUT, $this->time);
+        }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
         if (curl_exec($ch) === false) {
@@ -99,10 +208,20 @@ class Request
         } else {
             $data = curl_multi_getcontent($ch);
         }
-        $this->setResult($data);
-        $this->setCode(curl_getinfo($ch, CURLINFO_HTTP_CODE));
+        $this->result = $data;
+        $this->code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
-
-        return $data;
+        $res = '';
+        if ($this->isHeader) {
+            $data = explode("\r\n\r\n", $data);
+            if (count($data > 1)) {
+                $resHeader = $data[0];
+                $res = $data[1];
+                $this->resHeader = $resHeader;
+            }
+        } else {
+            $res = $data;
+        }
+        return $res;
     }
 }
